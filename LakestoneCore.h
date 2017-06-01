e93f377f32bfbfdff90389b562c7ea753713d23c
ambientlight <primary.taras.vozniuk@gmail.com>
2017-06-01 18:08:35 +0800

    Restructuring with swift cocoa touch project added into repository

---
+++ LakestoneCore.h
@@ -0,0 +1,19 @@
+//
+//  LakestoneCore.h
+//  LakestoneCore
+//
+//  Created by Taras Vozniuk on 31/05/2017.
+//  Copyright © 2017 GeoThings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LakestoneCore.
+FOUNDATION_EXPORT double LakestoneCoreVersionNumber;
+
+//! Project version string for LakestoneCore.
+FOUNDATION_EXPORT const unsigned char LakestoneCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LakestoneCore/PublicHeader.h>
+
+
